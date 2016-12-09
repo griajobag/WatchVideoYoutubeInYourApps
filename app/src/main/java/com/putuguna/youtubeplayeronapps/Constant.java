08497c4760827e39a53d3c99ec312e2a8d6a4d9e
putuguna <putuguna@appschef.com>
2016-12-09 18:09:30 +0800

    putuguna : add project

---
+++ app/src/main/java/com/putuguna/youtubeplayeronapps/Constant.java
@@ -0,0 +1,10 @@
+package com.putuguna.youtubeplayeronapps;
+
+/**
+ * Created by putuguna on 09/12/16.
+ */
+
+public class Constant {
+    public static final String YOUTUBE_API_KEY = "AIzaSyCdPy5ZUkJ1arINjSEHmGx0SgrjlctOTwU";
+    public static final String YOUTUBE_ID = "WhRaCA9bC9k";
+}
---
+++ app/src/main/java/com/putuguna/youtubeplayeronapps/MainActivity.java
@@ -0,0 +1,73 @@
+package com.putuguna.youtubeplayeronapps;
+
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class MainActivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener {
+
+    private static final int REQUEST_NUMBER = 999;
+    private YouTubePlayerView youTubePlayerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        youTubePlayerView = (YouTubePlayerView) findViewById(R.id.youtube_player);
+
+        //set the api key here
+        youTubePlayerView.initialize(Constant.YOUTUBE_API_KEY,this);
+    }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+        if(!b){
+            youTubePlayer.cueVideo(Constant.YOUTUBE_ID);
+            /**
+             * there are 2 method you can user here :
+             * .cueVideo(), for didn't play automatically
+             * .loadVideo(), for do play automatically
+             *
+             * if you are using play automatically, it better if you hide the video controllers
+             * do like below :
+             * youTubePlayer.setPlayerStyle(YouTubePlayer.PlayerStyle.CHROMELESS);//hide the players controllers
+             */
+        }
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+        if(youTubeInitializationResult.isUserRecoverableError()){
+            youTubeInitializationResult.getErrorDialog(this, REQUEST_NUMBER).show();
+        }else{
+            String errorMessage = String.format(
+                    "There was an error initializing the YouTubePlayer (%1$s)", youTubeInitializationResult.toString()
+            );
+            Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==REQUEST_NUMBER){
+            youTubePlayerView.initialize(Constant.YOUTUBE_API_KEY,this);
+        }
+    }
+
+}
